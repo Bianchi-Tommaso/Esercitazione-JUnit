997bb938f8a50997706cf8746950c08cc6f7eb73
lai2 <informatica@lai2>
2021-01-19 09:35:03 +0100

    Esercitazione JUnit

---
+++ src/main/java/NumberHelper.java
@@ -0,0 +1,72 @@
+
+public class NumberHelper 
+{
+    private int number;
+
+    public NumberHelper(int n) 
+    {
+        number = n;
+    }
+
+    public boolean isEven() 
+    {
+        boolean controllo = false;
+        
+        
+        // ritorna true se number numero pari, altrimenti ritorna false
+        if(number % 2 == 0)
+            controllo = true;
+        
+        return controllo;
+    }
+
+    public boolean isOdd() 
+    {
+        // ritorna true se number è un numero dispari, altrimenti ritorna false
+        
+        boolean controllo = true;
+        
+        
+        // ritorna true se number numero pari, altrimenti ritorna false
+        if(number % 2 == 0)
+            controllo = false;
+        
+        return controllo;
+    }
+    
+    public int sum(int n)
+    {
+        // ritorna la somma tra number e n
+        
+        int somma = 0;
+        
+        return somma = n + number;
+        
+    }
+
+    public boolean isPrime() 
+    {
+        // ritorna true se number è un numero primo, altrimenti ritorna false
+      boolean controllo = true;
+      int i = 0;
+      
+    for(i = 2; i < number/2; i++)
+        if(number % i == 0)
+            controllo = false;
+    return controllo;
+}
+        
+        
+
+    public boolean isDisisibleBy(int n) 
+    {
+        // ritorna true se number è divisibile per il parametro n, altrimenti ritorna false
+        
+        boolean controllo = false;
+        
+        if(number / n % 2 == 0)
+            controllo = true;
+        
+        return controllo;
+    }
+}
---
+++ src/test/java/NumberHelperTest.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author informatica
+ */
+public class NumberHelperTest {
+    
+    public NumberHelperTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isEven method, of class NumberHelper.
+     */
+    @Test
+    public void testIsEven() {
+      NumberHelper instance = new NumberHelper(2);
+      
+        boolean expResult = true;
+        boolean result = instance.isEven();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isOdd method, of class NumberHelper.
+     */
+    @Test
+    public void testIsOdd() {
+       
+        NumberHelper instance = new NumberHelper(1);
+        boolean expResult = true;
+        boolean result = instance.isOdd();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     
+    }
+
+    /**
+     * Test of sum method, of class NumberHelper.
+     */
+    @Test
+    public void testSum() {
+     
+        int n = 5;
+        NumberHelper instance = new NumberHelper(8);
+        int expResult = 13;
+        int result = instance.sum(n);
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of isPrime method, of class NumberHelper.
+     */
+    @Test
+    public void testIsPrime() {
+        
+        NumberHelper instance = new NumberHelper(13);
+        boolean expResult = true;
+        boolean result = instance.isPrime();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of isDisisibleBy method, of class NumberHelper.
+     */
+    @Test
+    public void testIsDisisibleBy() {
+   
+        int n = 3;
+        NumberHelper instance = new NumberHelper(12);
+        boolean expResult = true;
+        boolean result = instance.isDisisibleBy(n);
+        assertEquals(expResult, result);
+       
+    }
+    
+}
